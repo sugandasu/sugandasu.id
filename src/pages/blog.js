ae6a05be982e59d4c92e77a17702a4ef39fa59a4
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-20 17:13:22 +0800

    create empty blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MainLayout from "../components/MainLayout";
+
+const Blog = () => {
+  return (
+    <MainLayout pageTitle="Blog">
+      <h1 className="page-title">Blog</h1>
+      <div className="page-content">
+        <p>Blog is currently empty...</p>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Blog;
