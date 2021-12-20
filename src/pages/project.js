69e6b991287cfb7193305d063ae5cd6477e87438
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-20 17:13:36 +0800

    create empty project page

---
+++ src/pages/project.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MainLayout from "../components/MainLayout";
+
+const Project = () => {
+  return (
+    <MainLayout pageTitle="Project">
+      <h1 className="page-title">Project</h1>
+      <div className="page-content">
+        <p>Project is currently empty...</p>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Project;
